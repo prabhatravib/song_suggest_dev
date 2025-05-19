abe541f8f8b2d461e16fe4ebfb4e07b47700dfae
prabhatravib <prabhatravib@gmail.com>
2025-05-19 14:40:33 -0400

    Update youtube_service.py

--- youtube_service.py
+++ youtube_service.py
@@ -2,7 +2,7 @@ import os
 from flask import session
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-import re
+
 
 def create_youtube_client():
     """
@@ -42,70 +42,70 @@ class YouTubeService:
             request = self.client.playlists().list_next(request, response)
         return playlists
 
-def get_playlist_items(self, playlist_id: str):
-    """
-    Fetch all videos in a given YouTube playlist with enhanced metadata:
-    - Basic info (id, title/name, channel)
-    - Publication date
-    - Description
-    - Tags/keywords
-    - Topic categories
-    """
-    items = []
-    # First, get all playlist items
-    request = self.client.playlistItems().list(
-        part='snippet,contentDetails',
-        playlistId=playlist_id,
-        maxResults=50
-    )
-    
-    while request:
-        response = request.execute()
-        video_ids = []
-        temp_items = {}
+    def get_playlist_items(self, playlist_id: str):
+        """
+        Fetch all videos in a given YouTube playlist with enhanced metadata:
+        - Basic info (id, title/name, channel)
+        - Publication date
+        - Description
+        - Tags/keywords
+        - Topic categories
+        """
+        items = []
+        # First, get all playlist items
+        request = self.client.playlistItems().list(
+            part='snippet,contentDetails',
+            playlistId=playlist_id,
+            maxResults=50
+        )
         
-        # Process basic playlist item info
-        for item in response.get('items', []):
-            snippet = item['snippet']
-            video_id = snippet['resourceId']['videoId']
-            video_ids.append(video_id)
+        while request:
+            response = request.execute()
+            video_ids = []
+            temp_items = {}
             
-            # Store basic info in temporary dictionary
-            temp_items[video_id] = {
-                'id': video_id,
-                'name': snippet['title'],
-                'channel': snippet['channelTitle'],
-                'published_at': snippet['publishedAt'],
-                'description': snippet['description'],
-                'album': ''  # YouTube videos don't have album info
-            }
-        
-        # Batch request additional video details in chunks of 50
-        for i in range(0, len(video_ids), 50):
-            chunk = video_ids[i:i+50]
-            video_response = self.client.videos().list(
-                part='snippet,topicDetails',
-                id=','.join(chunk)
-            ).execute()
+            # Process basic playlist item info
+            for item in response.get('items', []):
+                snippet = item['snippet']
+                video_id = snippet['resourceId']['videoId']
+                video_ids.append(video_id)
+                
+                # Store basic info in temporary dictionary
+                temp_items[video_id] = {
+                    'id': video_id,
+                    'name': snippet['title'],
+                    'channel': snippet['channelTitle'],
+                    'published_at': snippet['publishedAt'],
+                    'description': snippet['description'],
+                    'album': ''  # YouTube videos don't have album info
+                }
             
-            # Process additional video details
-            for video in video_response.get('items', []):
-                video_id = video['id']
-                if video_id in temp_items:
-                    # Add tags
-                    temp_items[video_id]['tags'] = video['snippet'].get('tags', [])
-                    
-                    # Add topic categories if available
-                    if 'topicDetails' in video:
-                        topic_categories = video['topicDetails'].get('relevantTopicIds', [])
-                        temp_items[video_id]['topic_categories'] = topic_categories
-                    else:
-                        temp_items[video_id]['topic_categories'] = []
-        
-        # Add all processed items to the result list
-        items.extend(temp_items.values())
-        
-        # Get next page of results if available
-        request = self.client.playlistItems().list_next(request, response)
-        
-    return items
+            # Batch request additional video details in chunks of 50
+            for i in range(0, len(video_ids), 50):
+                chunk = video_ids[i:i+50]
+                video_response = self.client.videos().list(
+                    part='snippet,topicDetails',
+                    id=','.join(chunk)
+                ).execute()
+                
+                # Process additional video details
+                for video in video_response.get('items', []):
+                    video_id = video['id']
+                    if video_id in temp_items:
+                        # Add tags
+                        temp_items[video_id]['tags'] = video['snippet'].get('tags', [])
+                        
+                        # Add topic categories if available
+                        if 'topicDetails' in video:
+                            topic_categories = video['topicDetails'].get('relevantTopicIds', [])
+                            temp_items[video_id]['topic_categories'] = topic_categories
+                        else:
+                            temp_items[video_id]['topic_categories'] = []
+            
+            # Add all processed items to the result list
+            items.extend(temp_items.values())
+            
+            # Get next page of results if available
+            request = self.client.playlistItems().list_next(request, response)
+            
+        return items
